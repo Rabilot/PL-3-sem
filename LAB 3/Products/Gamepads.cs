402571a0a402b68a695fe94aea01ca24beb9d684
Rabilot <63284814+Rabilot@users.noreply.github.com>
2020-11-18 19:05:54 +0300

    Описал геймпад

--- LAB 3/Products/Gamepads.cs
+++ LAB 3/Products/Gamepads.cs
@@ -1,7 +1,22 @@
 ﻿namespace LAB_3
 {
-    public class Gamepads
+    public class Gamepads : Product
     {
-        
+        /// <summary>
+        /// Количество клавиш
+        /// </summary>
+        public int NumberOfKeys;
+        /// <summary>
+        /// Беспроводной ли геймпад
+        /// </summary>
+        public bool IsWireless;
+        /// <summary>
+        /// Есть ли аналоговые триггеры
+        /// </summary>
+        public bool AnalogTriggers;
+        /// <summary>
+        /// Есть ли тачпад
+        /// </summary>
+        public bool Touchpad;
     }
 }
