4cef0141454f3d4463231880fea23dbdd701e98a
Rabilot <63284814+Rabilot@users.noreply.github.com>
2020-11-18 19:30:03 +0300

    Добавил Wire

--- LAB 3/Products/Wire.cs
+++ LAB 3/Products/Wire.cs
@@ -2,6 +2,17 @@
 {
     public class Wire
     {
-        
+        /// <summary>
+        /// Длина провода
+        /// </summary>
+        public double Length;
+        /// <summary>
+        /// Входной разъём
+        /// </summary>
+        public string TypeIn;
+        /// <summary>
+        /// Выходной разъём
+        /// </summary>
+        public string TypeOut;
     }
 }
